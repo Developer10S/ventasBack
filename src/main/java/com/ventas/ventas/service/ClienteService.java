00fcb192ed8cce132b0f4ee6c8d56d2a0a263400
Sppy A.R <developer10@shareppy.com>
2022-06-22 14:55:02 -0500

    se realizó la creacion de las excepciones y los metodos

---
+++ src/main/java/com/ventas/ventas/service/ClienteService.java
@@ -0,0 +1,41 @@
+package com.ventas.ventas.service;
+
+import com.ventas.ventas.model.Cliente;
+import com.ventas.ventas.repository.IClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ClienteService implements IClienteService {
+
+    @Autowired
+    private IClienteRepository clienteRepor;
+
+    @Override
+    public List<Cliente> finAll() {
+        return clienteRepor.findAll();
+    }
+
+    @Override
+    public Optional<Cliente> findbyid(Integer id) {
+        return clienteRepor.findById(id);
+    }
+
+    @Override
+    public Cliente create(Cliente cliente) {
+        return clienteRepor.save(cliente);
+    }
+
+    @Override
+    public Cliente update(Cliente cliente) {
+        return clienteRepor.save(cliente);
+    }
+
+    @Override
+    public void delete(Integer id) {
+        clienteRepor.deleteById(id);
+    }
+}
